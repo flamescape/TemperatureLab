b797a767e5806f8485f0d2dc9a1889ec001bd7a9
John Ballantyne <johnballantyne@gmail.com>
2013-01-22 17:14:37 -0500

    Slow down update frequency
    Now 1s instead of 100ms

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ function updateReading() {
     lastReading = thermometer.read();
     console.log(lastReading);
 }
-setInterval(updateReading, 100);
+setInterval(updateReading, 1000);
 
 var app = express();
 var httpServer = http.createServer(app);
@@ -20,7 +20,7 @@ app.use(express.static(__dirname + '/web'));
 sockServer.on('connection', function(client) {
     var repeat = setInterval(function(){
         client.emit('update', lastReading);
-    }, 100);
+    }, 1000);
     client.on('disconnect', function(){
         clearInterval(repeat);
     });
