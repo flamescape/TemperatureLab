1323e25e5501d537ad5f093109b11c760260f4a2
Mikuso <g_github@flamescape.com>
2013-01-25 22:28:24 +0000

    Make it do stuff now.

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 var http = module.require('http');
 var express = require('express');
-var sio = module.require('socket.io');
+var io = module.require('socket.io');
 var util = module.require('util');
-var thermometer = module.require('thermometer_test')
-
-var lastReading = [];
-function updateReading() {
-    lastReading = thermometer.read();
-    console.log(lastReading);
-}
-setInterval(updateReading, 1000);
+var thermometers = module.require('thermometers');
 
 var app = express();
 var httpServer = http.createServer(app);
-var sockServer = sio.listen(httpServer);
+var sockServer = io.listen(httpServer);
+
+var thermoLab = new thermometers.ThermometerLab('/sys/bus/w1/devices/');
 
 app.use(express.static(__dirname + '/web'));
 
 sockServer.on('connection', function(client) {
-    var repeat = setInterval(function(){
-        client.emit('update', lastReading);
-    }, 1000);
-    client.on('disconnect', function(){
-        clearInterval(repeat);
+    var onNewThermometer = function(t){
+        ['tConnect', 'tData', 'tFail', 'tDisconnect'].forEach(function(evt){
+            t.on(evt, function(data) { client.emit(evt, data); });
+        });
+    };
+
+    var devs = thermoLab.getDevices();
+    for (var d in devs) { onNewThermometer(devs[d]); } // Relay all currently connected thermometers.
+    thermoLab.on('connect', onNewThermometer); // Relay all future connected thermometers.
+    client.once('disconnect', function(){ // Remove event listener when client disconnects.
+        thermoLab.removeListener('connect', onNewThermometer);
     });
 });
 
