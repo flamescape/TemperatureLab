3d6984e9261d751dbb984f96aface2572e185a19
John Ballantyne <johnballantyne@gmail.com>
2013-01-26 13:20:18 -0500

    Add basic database
    ...That does nothing

--- server.js
+++ server.js
@@ -2,15 +2,29 @@ var http = module.require('http');
 var express = require('express');
 var io = module.require('socket.io');
 var util = module.require('util');
+var sqlite3 = require('sqlite3').verbose();
 var thermometers = module.require('thermometers');
 
 var app = express();
 var httpServer = http.createServer(app);
 var sockServer = io.listen(httpServer, {log: false});
+var db = new sqlite3.Database('db.sql');
 
 var thermoLab = new thermometers.ThermometerLab('/sys/bus/w1/devices/');
 
 app.use(express.static(__dirname + '/web'));
+
+db.serialize(function() {
+  db.run("CREATE TABLE temps (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+});
+
+db.close();
 
 sockServer.on('connection', function(client) {
     var onNewThermometer = function(t){
